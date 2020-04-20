a6c45c30e986e783ff570afcfdae1dae28b373c3
sunlei <992023879@qq.com>
2020-04-20 11:30:47 +0800

    fix bug

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -7,7 +7,7 @@
   </div>
 </template>
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator'
+import { Vue, Component } from "vue-property-decorator";
 @Component
 export default class NotFound extends Vue {}
 </script>
