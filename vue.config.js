3bdaf35e869b40fd50c4732029f5fbc8ca81e06b
Diaz <diaz_zeng@outlook.com>
2020-04-21 22:12:10 +0800

    update project config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   configureWebpack: {
     devtool: "source-map"
-  }
+  },
+  outputDir: "dist",
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/company-site-frontend/" : "/"
 };
